503f946f4c912da57fd172ff7ff66655577cc1b8
SpayL <singlapayal43@gmail.com>
2019-03-26 10:20:41 +0530

    main defined

--- calculator.cpp
+++ calculator.cpp
@@ -13,4 +13,29 @@ class test
     {
         cout<<"Enter second number\n";
     }
-  };
+ };
+
+int main()
+{
+    test z;
+    int choice;
+    cout<<"1.Addition\n2.Subtraction\n3.Multiplication\n4.Divition\n";
+    cin>>choice;
+    switch (choice){
+        case 1:
+
+         case 2:
+         
+
+         case 3:
+      
+
+         case 4:
+
+         
+	case 5:
+
+           }
+return 0;
+}
+
